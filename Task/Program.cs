95f3074197498544a216efac7936e3923c68f447
iegorov99 <egorov.1999.ilya@yandex.ru>
2023-03-15 09:54:41 +0300

    Задача 19 - HARD необязательная

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19 - HARD необязательная
+
+// Напишите программу, которая принимает на вход целое число любой разрядности и проверяет, является ли оно палиндромом. Через строку нельзя решать само собой.
+
+int Num(int num)
+{
+    int num1 = num;
+    int revers = 0;
+    int ost = 0;
+    while (num > 0)
+    {
+        ost = num % 10;
+        revers = revers * 10 + ost;
+        num = num / 10;
+    }
+    return revers;
+}
+
+string Palindrom(int num)
+{
+    string res = "";
+    if (num == Num(num))
+    {
+        res = "Введённое число является палиндромом";
+        return res;
+    }
+    else
+    {
+        res = "Введённое число не является палиндромом";
+        return res;
+    }
+}
+
+try
+{
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Palindrom(num));
+}
+catch
+{
+    Console.WriteLine("Введено некорректное число");
+}
